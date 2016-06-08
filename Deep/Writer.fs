2f2b9362831558e4acf4acf7abe9ac7275c851f6
František Hliva <Frantiek Hliva>
2016-06-08 11:49:13 +0200

    Writer.printf

---
+++ Deep/Writer.fs
@@ -0,0 +1,15 @@
+﻿module Deep.Writer
+
+open System
+open System.IO
+
+let private printToWriter s (writer : TextWriter) =
+    writer.Write(s |> box :?> string)
+    writer
+
+let private printToWriterLineEnd s (writer : TextWriter) =
+    writer.Write(s |> box :?> string + Environment.NewLine)
+    writer
+
+let printf format = Printf.ksprintf printToWriter format
+let printfn format = Printf.ksprintf printToWriterLineEnd format
