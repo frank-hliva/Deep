971b9419e033566d6dda75c2077a4c5701aa4f2b
Frank Hliva <hlivafrank@gmail.com>
2019-12-11 12:29:32 +0100

    master

--- Deep/Async.fs
+++ Deep/Async.fs
@@ -5,5 +5,15 @@ open System.Threading.Tasks
 open Microsoft.FSharp
 
 type Control.AsyncBuilder with
+
     member async.Bind(t : Task<'T>, f : 'T -> Async<'R>) : Async<'R> = 
         async.Bind(Async.AwaitTask t, f)
+
+    member async.Bind(t : Task, f : unit -> Async<unit>) : Async<unit> =
+        async.Bind(Async.AwaitTask t, f)
+
+    member async.ReturnFrom(t : Task<'T>) : Async<'T> =
+        async.ReturnFrom(Async.AwaitTask t)
+
+    member async.ReturnFrom(t : Task) : Async<unit> =
+        async.ReturnFrom(Async.AwaitTask t)
